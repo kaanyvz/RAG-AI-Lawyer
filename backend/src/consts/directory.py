fb40ddcf5ec94bf6c590e21889e9dafa05a69c40
kaanyvz <kaan_yavuz12@hotmail.com>
2024-03-24 16:08:36 +0300

    folder directory and logger constants

---
+++ backend/src/consts/directory.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+ROOT_DIRECTORY = Path(__file__).resolve().parents[2]
+DATA_DIRECTORY = ROOT_DIRECTORY / "data"
+SOURCE_DIRECTORY = ROOT_DIRECTORY / "src"
+ROUTES_DIRECTORY = ROOT_DIRECTORY / "routes"
+
+
+
+
---
+++ backend/src/consts/log.py
@@ -0,0 +1,1 @@
+LOG_FORMAT = "%(asctime)s:%(pathname)s:%(levelname)s:%(lineno)d:%(message)s"
